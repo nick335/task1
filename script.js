3c43f3937effd55cdb2a0b900e8e1238e44e5b5b
Nicholas <102188095+nick335@users.noreply.github.com>
2023-09-09 14:01:31 +0100

    final commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const DayTag = document.getElementById('day')
 const timeTag= document.getElementById('time')
+// get day function
 function getDayName(){
  date = new Date() 
  locale = 'en-NG'
  return date.toLocaleDateString(locale, {weekday: 'long'})
 }
+// get currentUTCTime down to milliseconds
 function GetCurrentUTCTime(){
  const now = new Date();
  const hours = now.getUTCHours()
@@ -15,6 +17,7 @@ function GetCurrentUTCTime(){
 }
 const day = getDayName()
 const currentTime = GetCurrentUTCTime()
+// diplay on html
 DayTag.innerHTML = `Day of the week: ${day}`
 timeTag.innerHTML = `currentUTCTime: ${currentTime}`
 
